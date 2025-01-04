fd77465159b84511a10bc93c1a21d5ced4f7ac41
Joshua Rodriguez <joshrod1089@gmail.com>
2025-01-04 20:37:42 +0000

    Fixed to be whole folder

---
+++ target/debug/deps/string_ends_with.d
@@ -0,0 +1,5 @@
+C:\Users\joshr\OneDrive\Documents\Projects\Competitive Programming\Codewars\string_ends_with\target\debug\deps\string_ends_with.exe: src/main.rs
+
+C:\Users\joshr\OneDrive\Documents\Projects\Competitive Programming\Codewars\string_ends_with\target\debug\deps\string_ends_with.d: src/main.rs
+
+src/main.rs:
