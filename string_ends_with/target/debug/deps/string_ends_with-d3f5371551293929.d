12c625223ce01c9f0916e0be8a6d31f155e20259
Joshua Rodriguez <joshrod1089@gmail.com>
2025-01-08 21:27:52 +0000

    Actually fixed to be whole folder

---
+++ string_ends_with/target/debug/deps/string_ends_with-d3f5371551293929.d
@@ -0,0 +1,5 @@
+C:\Users\joshr\OneDrive\Documents\Projects\Competitive Programming\Codewars\string_ends_with\target\debug\deps\libstring_ends_with-d3f5371551293929.rmeta: src/main.rs
+
+C:\Users\joshr\OneDrive\Documents\Projects\Competitive Programming\Codewars\string_ends_with\target\debug\deps\string_ends_with-d3f5371551293929.d: src/main.rs
+
+src/main.rs:
