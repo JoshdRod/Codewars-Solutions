7fc2ef462ec264b9fc5a1e57c307e0c2371f1c30
Joshua Rodriguez <joshrod1089@gmail.com>
2025-01-08 22:07:32 +0000

    Find the divisors

---
+++ find_the_divisors/target/debug/deps/find_the_divisors-70bdeb7625ef50e0.d
@@ -0,0 +1,5 @@
+C:\Users\joshr\OneDrive\Documents\Projects\Competitive Programming\Codewars\find_the_divisors\target\debug\deps\libfind_the_divisors-70bdeb7625ef50e0.rmeta: src/main.rs
+
+C:\Users\joshr\OneDrive\Documents\Projects\Competitive Programming\Codewars\find_the_divisors\target\debug\deps\find_the_divisors-70bdeb7625ef50e0.d: src/main.rs
+
+src/main.rs:
