5f278a0974ca74885dc817c8773a5b177ee6c86b
Joshua Rodriguez <joshrod1089@gmail.com>
2025-01-08 21:28:38 +0000

    again

--- target/debug/deps/string_ends_with-aa804bf012c01f70.d
+++
@@ -1,5 +0,0 @@
-C:\Users\joshr\OneDrive\Documents\Projects\Competitive Programming\Codewars\string_ends_with\target\debug\deps\libstring_ends_with-aa804bf012c01f70.rmeta: src/main.rs
-
-C:\Users\joshr\OneDrive\Documents\Projects\Competitive Programming\Codewars\string_ends_with\target\debug\deps\string_ends_with-aa804bf012c01f70.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/string_ends_with.d
+++
@@ -1,5 +0,0 @@
-C:\Users\joshr\OneDrive\Documents\Projects\Competitive Programming\Codewars\string_ends_with\target\debug\deps\string_ends_with.exe: src/main.rs
-
-C:\Users\joshr\OneDrive\Documents\Projects\Competitive Programming\Codewars\string_ends_with\target\debug\deps\string_ends_with.d: src/main.rs
-
-src/main.rs:
